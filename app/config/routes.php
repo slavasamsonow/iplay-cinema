63631ba5ec80dbf7c348e70375edabeb2df16c7f
slavasamsonow <slavasamsonow@yandex.ru>
2018-09-21 18:43:55 +0400

    Добавлены страницы работы с личным кабинетом

--- app/config/routes.php
+++ app/config/routes.php
@@ -5,4 +5,31 @@ return [
         'controller' => 'main',
         'action' => 'index',
     ],
+
+    // userController
+    'login' => [
+        'controller' => 'account',
+        'action' => 'login',
+    ],
+    'register' => [
+        'controller' => 'account',
+        'action' => 'register',
+    ],
+    'confirmation' => [
+        'controller' => 'account',
+        'action' => 'confirmation',
+    ],
+    'account' => [
+        'controller' => 'account',
+        'action' => 'index',
+    ],
+    'account/logout' => [
+        'controller' => 'account',
+        'action' => 'logout',
+    ],
+    'account/editpass' => [
+        'controller' => 'account',
+        'action' => 'editpassword',
+    ],
+
 ];
