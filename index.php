3d1a46d82374ad3d8ea5bd67b24a2d0733f53b78
slavasamsonow <slavasamsonow@yandex.ru>
2018-09-21 11:32:07 +0400

    Основные части работы программы

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require 'app/lib/Dev.php';
+
+use app\core\Router;
+
+spl_autoload_register(function($class){
+    $path = str_replace('\\','/', $class.'.php');
+    if(file_exists($path)){
+        require $path;
+    }
+});
+
+session_start();
+
+$router = new Router;
+$router->run();
+
+?>
