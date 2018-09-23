b3f86bdd59f0b47d5b04e7444ef60a68a7313d5c
slavasamsonow <slavasamsonow@yandex.ru>
2018-09-24 03:00:30 +0400

    Подгружаем стили в шаблоне, а не в стилях

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -17,6 +17,7 @@
 
         <link rel="stylesheet" href="/public/css/bootstrap.min.css">
         <link rel="stylesheet" href="/public/css/main.css">
+        <link rel="stylesheet" href="/public/css/media.css">
 
         <script src="/public/js/jquery.js"></script>
         <script src="/public/js/jquery.maskedinput.min.js"></script>
