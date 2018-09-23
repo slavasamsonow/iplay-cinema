3e27c40bf97f151c038936f86da2f5af0ae49dfc
slavasamsonow <slavasamsonow@yandex.ru>
2018-09-24 00:37:51 +0400

    Стандартный шаблон

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -70,24 +70,8 @@
             </div>
             <div class="navbar-right">
                 <ul>
-                    <!-- <li>
-                        <a href="#">Новости</a>
-                    </li>
                     <li>
-                        <a href="#">Статьи</a>
-                    </li> -->
-                    <li>
-                        <a href="/login" class="lk">
-                            <?php
-                                if(isset($user['fname']) || isset($user['lname'])){
-                                    echo $user['fname'].' '.$user['lname'];
-                                }elseif(isset($user['username'])){
-                                    echo $user['username'];
-                                }else{
-                                    echo 'Личный кабинет';
-                                }
-                            ?>
-                        </a>
+                        <a href="/login" class="lk">Личный кабинет</a>
                     </li>
                 </ul>
             </div>
@@ -102,22 +86,12 @@
 
         </div>
     </div>
-
-    <footer>
+    <footer class="transparent">
         <div class="container-fluid">
-            <div class="col-md-3">
-                &#169; Киношкола iPlay, 2015-<?=date('Y', time())?> <br>
-                <a href="mailto:videolab.play@gmail.com">videolab.play@gmail.com</a>
-            </div>
-            <div class="col-md-3 col-md-offset-3">
-                <a href="/public/docs/protect_policy_of_personal_information.pdf" target="_blank">Политика конфеденциальности</a>
-                <hr>
-                ИП Муратова Татьяна Сергеевна <br>
-                ИНН 183402585126 <br>
-                ОГРНИП 310184015100083
-            </div>
+            <a href="/public/docs/protect_policy_of_personal_information.pdf" target="_blank">Политика конфеденциальности</a>
         </div>
     </footer>
+
     <div class="modal message">
         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
         <div class="modal-header"></div>
