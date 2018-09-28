062edb98ee2b332cd54708a790ce02a5bd77731d
slavasamsonow <slavasamsonow@yandex.ru>
2018-09-28 23:18:49 +0400

    Добавлены ссылки на страницы

--- app/views/account/users.php
+++ app/views/account/users.php
@@ -1,8 +1,12 @@
 <div class="container-fluid">
     <h1>Все пользователи</h1>
     <? foreach($users as $user):?>
-        <div class="user">
-            <div class="name"><?=$user['fname'].' '.$user['lname']?></div>
-        </div>
+    <div class="user">
+        <a href="/user/<?=$user['username']?>">
+            <div class="name">
+                <?=$user['fname'].' '.$user['lname']?>
+            </div>
+        </a>
+    </div>
     <? endforeach ?>
 </div>
