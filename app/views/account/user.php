a73c45913002dc21ead21a3ffaf9ce7518bf35d8
slavasamsonow <slavasamsonow@yandex.ru>
2018-09-28 23:19:12 +0400

    Выводится текст о человеке. Если есть видео, вставляется видео (шаблон)

--- app/views/account/user.php
+++ app/views/account/user.php
@@ -1,4 +1,9 @@
 <div class="container-fluid">
 <h1><?=$userPage['fname'].' '.$userPage['lname']?></h1>
+<p><?=$userPage['about']?></p>
+
+<?php if(!empty($userPage['video'])):?>
+Здесь может быть ваше видео
+<? endif ?>
 
 </div>
