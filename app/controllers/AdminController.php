8dcc4c0776c74c7579054486ecae795652d5aee9
slavasamsonow <slavasamsonow@yandex.ru>
2018-09-28 13:16:38 +0400

    Проверка при изменении роли, чтобы только имеющие роль могли менять

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -6,12 +6,12 @@ use app\core\Controller;
 
 class AdminController extends Controller{
     public function confirmTasksAction(){
-        if(!empty($_POST)){
-            $data = $this->model->saveStatusTask($_POST);
-            echo json_encode(['data' => $data]);
-            exit();
-        }
         if($this->model->role == 'admin'){
+            if(!empty($_POST)){
+                $data = $this->model->saveStatusTask($_POST);
+                echo json_encode(['data' => $data]);
+                exit();
+            }
             $tasks = $this->model->getNoverifyTasks();
             $vars = [
                 'tasks' => $tasks,
