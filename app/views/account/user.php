4c19940d13ea3d75cd4e56858f0b026c125e3e04
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-01 21:45:07 +0400

    Если есть фото, оно отображается

--- app/views/account/user.php
+++ app/views/account/user.php
@@ -1,5 +1,10 @@
 <div class="container-fluid">
 <h1><?=$userPage['fname'].' '.$userPage['lname']?></h1>
+<?php if(!empty($userPage['photo'])): ?>
+<img src="/public/img/users/<?=$userPage['photo']?>" alt="" width="200">
+<?php else: ?>
+Нет фото
+<?php endif ?>
 <p><?=$userPage['about']?></p>
 
 <?php if(!empty($userPage['video'])):?>
