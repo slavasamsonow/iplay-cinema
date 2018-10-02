8a2d212ba6152b00cbe00f21be9be88e17fa85d4
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-02 18:57:12 +0400

    Добавлена фото профиля

--- app/views/account/users.php
+++ app/views/account/users.php
@@ -1,10 +1,17 @@
 <h1>Все пользователи</h1>
-<? foreach($users as $userItem):?>
-<div class="user">
-    <a href="/user/<?=$userItem['username']?>">
-        <div class="name">
-            <?=$userItem['fname'].' '.$userItem['lname']?>
-        </div>
-    </a>
+<div class="userList">
+    <? foreach($users as $userItem):?>
+    <div class="user">
+        <a href="/user/<?=$userItem['username']?>">
+            <div class="photo">
+                <? if(!empty($userItem['photo'])): ?>
+                <img src="/public/img/users/<?=$userItem['photo']?>" alt="">
+                <? endif ?>
+            </div>
+            <div class="name">
+                <?=$userItem['fname'].' '.$userItem['lname']?>
+            </div>
+        </a>
+    </div>
+    <? endforeach ?>
 </div>
-<? endforeach ?>
