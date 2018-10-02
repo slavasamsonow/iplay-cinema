588a2c2a21e94b0bdf7e9fa87098e875535583ac
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-02 15:44:08 +0400

    Убран container-fluid

--- app/views/account/editinfo.php
+++ app/views/account/editinfo.php
@@ -1,37 +1,34 @@
-<div class="container-fluid">
-    <h1>Изменение информации</h1>
-    <div class="col-md-3">
-        <form action="/account/editinfo" method="post" enctype="multipart/form-data">
-            <input type="hidden" name="oldphoto" value="<?=$user['photo']?>">
-            <div class="row">
-                <div class="control-group form-group col-md-6">
-                    <label>Имя:</label>
-                    <input type="text" class="form-control" name="fname" value="<?=$user['fname']?>" required="true">
-                </div>
-                <div class="control-group form-group col-md-6">
-                    <label>Фамилия:</label>
-                    <input type="text" class="form-control" name="lname" value="<?=$user['lname']?>" required="true">
-                </div>
-            </div>
-            <div class="control-group form-group">
-                    <label>Имя пользователя:</label>
-                    <input type="text" class="form-control" name="username" value="<?=$user['username']?>">
-                </div>
-            <div class="control-group form-group">
-                <label>Обо мне:</label>
-                <textarea class="form-control" name="about" id=""><?=$user['about']?></textarea>
-            </div>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" name="public" value="public" <? if($user['public'] == 1) echo 'checked';?>>
-                    <span> Публичный профиль </span>
-                </label>
-            </div>
-            <div class="div" class="control-group form-group">
-                <label>Загрузить новое фото:</label>
-                <input type="file" name="photo">
-            </div>
-            <button type="submit" class="btn btn-default">Сохранить данные</button>
-        </form>
+<h1>Изменение информации</h1>
+<form action="/account/editinfo" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="oldphoto" value="<?=$user['photo']?>">
+    <div class="row">
+        <div class="control-group form-group col-md-6">
+            <label>Имя:</label>
+            <input type="text" class="form-control" name="fname" value="<?=$user['fname']?>" required="true">
+        </div>
+        <div class="control-group form-group col-md-6">
+            <label>Фамилия:</label>
+            <input type="text" class="form-control" name="lname" value="<?=$user['lname']?>" required="true">
+        </div>
     </div>
-</div>
+    <div class="control-group form-group">
+        <label>Имя пользователя:</label>
+        <input type="text" class="form-control" name="username" value="<?=$user['username']?>">
+    </div>
+    <div class="control-group form-group">
+        <label>Обо мне:</label>
+        <textarea class="form-control" name="about" id=""><?=$user['about']?></textarea>
+    </div>
+    <div class="checkbox">
+        <label>
+            <input type="checkbox" name="public" value="public" <? if($user['public']==1) echo 'checked' ;?>>
+            <span> Публичный профиль </span>
+        </label>
+    </div>
+    <div class="div" class="control-group form-group">
+        <label>Загрузить новое фото:</label>
+        <input type="file" name="photo">
+    </div>
+    <br>
+    <button type="submit" class="btn btn-default">Сохранить данные</button>
+</form>
--- app/views/account/user.php
+++ app/views/account/user.php
@@ -1,14 +1,15 @@
-<div class="container-fluid">
-<h1><?=$userPage['fname'].' '.$userPage['lname']?></h1>
+<h1>
+    <?=$userPage['fname'].' '.$userPage['lname']?>
+</h1>
 <?php if(!empty($userPage['photo'])): ?>
 <img src="/public/img/users/<?=$userPage['photo']?>" alt="" width="200">
 <?php else: ?>
 Нет фото
 <?php endif ?>
-<p><?=$userPage['about']?></p>
+<p>
+    <?=$userPage['about']?>
+</p>
 
 <?php if(!empty($userPage['video'])):?>
 Здесь может быть ваше видео
 <? endif ?>
-
-</div>
