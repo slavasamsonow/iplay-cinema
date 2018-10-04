51c980939dbfdafb4b2825c2ca315b64497f680b
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-04 12:52:18 +0400

    Установлен пиксель ВК

--- app/views/layouts/pay.php
+++ app/views/layouts/pay.php
@@ -69,6 +69,9 @@
             gtag('config', 'UA-125492467-1');
         </script>
 
+        <!-- PIXEL VK -->
+        <script type="text/javascript">!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src="https://vk.com/js/api/openapi.js?159",t.onload=function(){VK.Retargeting.Init("VK-RTRG-290017-e59lI"),VK.Retargeting.Hit()},document.head.appendChild(t)}();</script><noscript><img src="https://vk.com/rtrg?p=VK-RTRG-290017-e59lI" style="position:fixed; left:-999px;" alt=""/></noscript>
+
     </head>
 
     <body>
