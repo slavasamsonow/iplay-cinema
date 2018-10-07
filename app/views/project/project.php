69a82c100d2c512d185c9c7e5cbf9c50a82c3898
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-07 13:50:35 +0400

    Страница проекта

---
+++ app/views/project/project.php
@@ -0,0 +1,14 @@
+<h1>
+    <?=$project['name']?>
+</h1>
+<div class="date-start">
+    <?=date('d.m.Y',$project['timestart'])?>
+</div>
+<div class="description">
+    <?=$project['description']?>
+</div>
+<div class="creator">Создатель:
+    <a href="/user/id<?=$project['creatorid']?>">
+        <?=$project['creatorfname'].' '.$project['creatorlname']?>
+    </a>
+</div>
