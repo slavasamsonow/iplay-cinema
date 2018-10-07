b0b338dc5e2981d2e5bee700ea379a92ec6e6cfd
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-07 13:58:05 +0400

    Сразу проверяются права на просмотр Страница создания проекта

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -9,21 +9,26 @@ class AdminController extends Controller{
     public function __construct($route){
         parent::__construct($route);
         $this->view->layout = 'lk';
+        if(!$this->model->role == 'admin'){
+            $this->view->errorCode('403');
+        }
     }
 
     public function confirmTasksAction(){
-        if($this->model->role == 'admin'){
-            if(!empty($_POST)){
-                $data = $this->model->saveStatusTask($_POST);
-                $this->view->data($data);
-            }
-            $tasks = $this->model->getNoverifyTasks();
-            $vars = [
-                'tasks' => $tasks,
-            ];
-            $this->view->render($vars);
-        }else{
-            $this->view->errorCode('403');
+        if(!empty($_POST)){
+            $data = $this->model->saveStatusTask($_POST);
+            $this->view->data($data);
         }
+        $tasks = $this->model->getNoverifyTasks();
+        $vars = [
+            'tasks' => $tasks,
+        ];
+        $this->view->render($vars);
+    }
+
+    public function addprojectAction(){
+        $vars = [
+        ];
+        $this->view->render($vars);
     }
 }
