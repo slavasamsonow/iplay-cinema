419abf578f424c5204cf75393d5a17a55abacff2
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-07 16:13:34 +0400

    Создание нового проекта

--- app/models/Project.php
+++ app/models/Project.php
@@ -9,14 +9,23 @@ class Project extends Model{
         return $this->db->row('SELECT p.id, p.name, p.description, p.timestart, u.id AS creatorid,u.fname AS creatorfname, u.lname AS creatorlname FROM projects p JOIN users u ON p.creator = u.id WHERE p.active = 1');
     }
 
-    public function project($projectif){
+    public function project($projectid){
         $params = [
-            'id' => $projectif,
+            'id' => $projectid,
         ];
         $project = $this->db->row('SELECT p.id, p.name, p.description, p.timestart, u.id AS creatorid, u.fname AS creatorfname, u.lname AS creatorlname FROM projects p JOIN users u ON p.creator = u.id WHERE p.active = 1 AND p.id = :id', $params);
         if(empty($project)){
             return false;
         }
         return $project[0];
+    }
+
+    public function createProject($indata){
+        $params = $this->textFormatting($indata);
+        $params['timestart'] = time();
+        $paramNandV = $this->db->paramNandV($params);
+
+        $this->db->query('INSERT INTO `projects` ('.$paramNandV['N'].') VALUES ('.$paramNandV['V'].')', $params);
+        return true;
     }
 }
