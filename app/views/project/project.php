09aa5eb24f114cd314a05593e7cec66fad806428
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-07 17:26:55 +0400

    Создатель выше описания

--- app/views/project/project.php
+++ app/views/project/project.php
@@ -4,11 +4,11 @@
 <div class="date-start">
     <?=date('d.m.Y',$project['timestart'])?>
 </div>
-<div class="description">
-    <?=$project['description']?>
-</div>
 <div class="creator">Создатель:
     <a href="/user/id<?=$project['creatorid']?>">
         <?=$project['creatorfname'].' '.$project['creatorlname']?>
     </a>
+</div>
+<div class="description">
+    <?=$project['description']?>
 </div>
--- app/views/project/projectslist.php
+++ app/views/project/projectslist.php
@@ -1,3 +1,4 @@
+<?php if(!empty($projects)): ?>
 <div class="projects row">
     <?php foreach($projects as $project):?>
     <div class="project col-md-4">
@@ -7,15 +8,18 @@
         <div class="date-start">
             <?=date('d.m.Y',$project['timestart'])?>
         </div>
-        <div class="description">
-            <?=$project['description']?>
-        </div>
         <div class="creator">Создатель:
             <a href="/user/id<?=$project['creatorid']?>">
                 <?=$project['creatorfname'].' '.$project['creatorlname']?>
             </a>
         </div>
+        <div class="description">
+            <?=$project['description']?>
+        </div>
         <a href="/project/<?=$project['id']?>" class="btn btn-default btn-sm">Подробнее</a>
     </div>
     <?php endforeach ?>
 </div>
+<?php else: ?>
+На данный момент нет активых проектов
+<?php endif?>
