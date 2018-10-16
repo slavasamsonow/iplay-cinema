5b47902949f19a50c161bea5941f7f9e214ccc02
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-16 13:43:57 +0400

    Вставляем видео проекта

--- app/views/project/project.php
+++ app/views/project/project.php
@@ -2,23 +2,27 @@
     <?=$project['name']?>
 </h1>
 <div class="row">
-    <? if(isset($project['image'])):?>
-    <div class="col-md-3"><img src="/public/img/projects/<?=$project['image']?>" alt="<?=$project['name']?>">
+    <? if($project['image']):?>
+    <div class="col-md-3">
+        <img src="/public/img/projects/<?=$project['image']?>" alt="<?=$project['name']?>">
     </div>
-    <div class="col-md-9">
-        <? else: ?>
-        <div class="col-md-12">
-            <? endif ?>
-            <div class="date-start">
-                <?=date('d.m.Y',$project['timestart'])?>
-            </div>
-            <div class="creator">Создатель:
-                <a href="/user/id<?=$project['creatorid']?>">
-                    <?=$project['creatorfname'].' '.$project['creatorlname']?>
-                </a>
-            </div>
-            <div class="description">
-                <?=$project['description']?>
-            </div>
+    <? endif ?>
+    <div class="<?php echo ($project['image'])?'col-md-9':'col-md-12'?>">
+        <div class="date-start">
+            <?=date('d.m.Y',$project['timestart'])?>
+        </div>
+        <div class="creator">Создатель:
+            <a href="/user/id<?=$project['creatorid']?>">
+                <?=$project['creatorfname'].' '.$project['creatorlname']?>
+            </a>
+        </div>
+        <div class="description">
+            <?=$project['description']?>
         </div>
     </div>
+</div>
+<?php if($project['video']): ?>
+<h2>Видео</h2>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/<?=$project['video']?>?rel=0&amp;controls=0&amp;showinfo=0"
+    frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<?php endif ?>
