ad0817e50efbc6ed6394d28bb503581f43d8ef6c
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-16 13:33:57 +0400

    Верстка страницы проекта

--- app/views/project/project.php
+++ app/views/project/project.php
@@ -1,14 +1,24 @@
 <h1>
     <?=$project['name']?>
 </h1>
-<div class="date-start">
-    <?=date('d.m.Y',$project['timestart'])?>
-</div>
-<div class="creator">Создатель:
-    <a href="/user/id<?=$project['creatorid']?>">
-        <?=$project['creatorfname'].' '.$project['creatorlname']?>
-    </a>
-</div>
-<div class="description">
-    <?=$project['description']?>
-</div>
+<div class="row">
+    <? if(isset($project['image'])):?>
+    <div class="col-md-3"><img src="/public/img/projects/<?=$project['image']?>" alt="<?=$project['name']?>">
+    </div>
+    <div class="col-md-9">
+        <? else: ?>
+        <div class="col-md-12">
+            <? endif ?>
+            <div class="date-start">
+                <?=date('d.m.Y',$project['timestart'])?>
+            </div>
+            <div class="creator">Создатель:
+                <a href="/user/id<?=$project['creatorid']?>">
+                    <?=$project['creatorfname'].' '.$project['creatorlname']?>
+                </a>
+            </div>
+            <div class="description">
+                <?=$project['description']?>
+            </div>
+        </div>
+    </div>
