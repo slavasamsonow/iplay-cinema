525fae097e2d117e20ae8c86962543f45926f19d
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-17 10:59:54 +0400

    Отмена закругления слайдера

--- public/js/main.js
+++ public/js/main.js
@@ -199,7 +199,6 @@ $(document).ready(function () {
 	$(".owl-carousel").owlCarousel({
 		items: 4,
 		margin: 20,
-		loop: true,
 		autoplay: true,
 		autoplayTimeout: 3000,
 		autoplayHoverPause: true,
