ec81094402bd51fdfb49d8e63db06e55e6da7f1b
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-20 13:39:07 +0400

    Новый файл стилей

--- app/views/layouts/intro.php
+++ app/views/layouts/intro.php
@@ -17,7 +17,7 @@
 
         <link rel="stylesheet" href="/public/css/bootstrap.min.css">
         <link rel="stylesheet" href="/public/css/default.css?v=0.7.0">
-        <link rel="stylesheet" href="/public/css/main.css?v=0.7.0">
+        <link rel="stylesheet" href="/public/css/main.css?v=0.7.2">
         <link rel="stylesheet" href="/public/css/media.css?v=0.7.0">
 
         <script src="/public/js/jquery.js"></script>
