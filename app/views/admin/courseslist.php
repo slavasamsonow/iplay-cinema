846876eb952c60b116545d7158c12e37594a9e7c
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-23 18:11:49 +0400

    Верстка страниц со списком проектов и курсов админа

--- app/views/admin/courseslist.php
+++ app/views/admin/courseslist.php
@@ -1,30 +1,39 @@
+<h1><span class="big">Курсы</span></h1>
+<a href="/admin/addcourse" class="btn">Создать новый курс</a>
 <?php if(!empty($courses)): ?>
-<div class="courses row">
-    <?php foreach($courses as $course):?>
-    <div class="course col-md-4">
-        <h2>
-            <?=$course['name']?>
-        </h2>
-        <div class="date-start">
-            Дата: <?=date('d.m.Y',$course['timestart'])?>
+<div class="courses">
+    <?php for($i = 0; $i < count($courses); $i++): ?>
+    <?php if($i % 3 == 0):?>
+    <div class="row">
+        <?php endif ?>
+        <div class="course col-md-4">
+            <h2>
+                <?=$courses[$i]['name']?>
+            </h2>
+            <div class="date-start">
+                Дата:
+                <?=date('d.m.Y',$courses[$i]['timestart'])?>
+            </div>
+            <div class="param">
+                Активный:
+                <?php if($courses[$i]['active'] == 1){echo 'Да';}else{echo 'Нет';}?> <br>
+                Приватный:
+                <?php if($courses[$i]['private'] == 1){echo 'Да';}else{echo 'Нет';}?> <br>
+                Оплата:
+                <?php if($courses[$i]['payment'] == 1){echo 'Да';}else{echo 'Нет';}?> <br>
+                Цена:
+                <?=$courses[$i]['price']?>
+            </div><br>
+            <div class="description">
+                <?=$courses[$i]['caption']?>
+            </div><br>
+            <!-- <a href="/project/<?=$courses[$i]['id']?>" class="btn btn-default btn-sm">Подробнее</a> -->
+            <a href="/admin/editcourse/<?=$courses[$i]['id']?>" class="btn btn-default btn-sm">Редактировать</a>
         </div>
-        <div class="date-start">
-            Цена:
-            <?=$course['price']?>
-        </div>
-        <div class="param">
-            Активный: <?php if($course['active'] == 1){echo 'Да';}else{echo 'Нет';}?> <br>
-            Приватный: <?php if($course['private'] == 1){echo 'Да';}else{echo 'Нет';}?> <br>
-            Оплата: <?php if($course['payment'] == 1){echo 'Да';}else{echo 'Нет';}?> <br>
-            Цена: <?=$course['price']?>
-        </div><br>
-        <div class="description">
-            <?=$course['caption']?>
-        </div><br>
-        <!-- <a href="/project/<?=$course['id']?>" class="btn btn-default btn-sm">Подробнее</a> -->
-        <a href="/admin/editcourse/<?=$course['id']?>" class="btn btn-default btn-sm">Редактировать</a>
+        <?php if($i % 3 == 2):?>
     </div>
-    <?php endforeach ?>
+    <?php endif ?>
+    <?php endfor?>
 </div>
 <?php else: ?>
 На данный момент нет курсов
--- app/views/admin/projectslist.php
+++ app/views/admin/projectslist.php
@@ -1,25 +1,34 @@
+<h1><span class="big">Проекты</span></h1>
+<a href="/admin/addproject" class="btn">Создать новый проект</a>
 <?php if(!empty($projects)): ?>
-<div class="projects row">
-    <?php foreach($projects as $project):?>
-    <div class="project col-md-4">
-        <h2>
-            <?=$project['name']?>
-        </h2>
-        <div class="date-start">
-            Дата создания: <?=date('d.m.Y',$project['timestart'])?>
+<div class="projects">
+    <?php for($i = 0; $i < count($projects); $i++): ?>
+    <?php if($i % 3 == 0):?>
+    <div class="row">
+        <?php endif ?>
+        <div class="project col-md-4">
+            <h2>
+                <?=$projects[$i]['name']?>
+            </h2>
+            <div class="date-start">
+                Дата создания:
+                <?=date('d.m.Y',$projects[$i]['timestart'])?>
+            </div>
+            <div class="creator">Создатель:
+                <a href="/user/id<?=$projects[$i]['creatorid']?>">
+                    <?=$projects[$i]['creatorfname'].' '.$projects[$i]['creatorlname']?>
+                </a>
+            </div>
+            <div class="description">
+                <?=$projects[$i]['caption']?>
+            </div>
+            <a href="/project/<?=$projects[$i]['id']?>" class="btn btn-default btn-sm">Подробнее</a>
+            <a href="/admin/editproject/<?=$projects[$i]['id']?>" class="btn btn-default btn-sm">Редактировать</a>
         </div>
-        <div class="creator">Создатель:
-            <a href="/user/id<?=$project['creatorid']?>">
-                <?=$project['creatorfname'].' '.$project['creatorlname']?>
-            </a>
-        </div>
-        <div class="description">
-            <?=$project['caption']?>
-        </div>
-        <a href="/project/<?=$project['id']?>" class="btn btn-default btn-sm">Подробнее</a>
-        <a href="/admin/editproject/<?=$project['id']?>" class="btn btn-default btn-sm">Редактировать</a>
+        <?php if($i % 3 == 2):?>
     </div>
-    <?php endforeach ?>
+    <?php endif ?>
+    <?php endfor?>
 </div>
 <?php else: ?>
 На данный момент нет проектов
