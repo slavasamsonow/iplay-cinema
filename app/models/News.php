b9d7866ee89ea633bb178890215194d8179d540a
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-24 14:04:08 +0400

    Прикрепляем имя и фамилию автора

--- app/models/News.php
+++ app/models/News.php
@@ -14,10 +14,13 @@ class News extends Model{
         $params = [
             'id' => $newsid
         ];
-        $news = $this->db->row('SELECT n.*, u.fname AS authorFname, u.lname AS authorLname, u.id AS authorId FROM news n JOIN users u ON n.author = u.id WHERE n.id = :id', $params);
+        $news = $this->db->row('SELECT n.*, u.fname, u.lname, u.id AS authorId FROM news n JOIN users u ON n.author = u.id WHERE n.id = :id', $params);
         if(empty($news)){
             return false;
         }
-        return $news[0];
+        $news = $news[0];
+        $news['author'] = $news['fname'].' '.$news['lname'];
+        unset($news['fname'],$news['lname']);
+        return $news;
     }
 }
