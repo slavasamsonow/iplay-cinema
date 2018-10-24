bad12bd841cac45a68c2bdce30c6dea2f744b1ca
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-24 13:43:50 +0400

    Контроллер новостей

---
+++ app/controllers/Newscontroller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\controllers;
+
+use app\core\Controller;
+
+class NewsController extends Controller{
+    public function newslistAction(){
+        $vars = [
+            'newslist' => $this->model->newsList(),
+        ];
+        $this->view->render($vars);
+    }
+    public function newsinfoAction(){
+        if(!$news = $this->model->newsInfo($this->route['newsid'])){
+            $this->view->errorCode(404);
+        }
+        $vars = [
+            'news' => $news,
+        ];
+        $this->view->render($vars);
+    }
+}
