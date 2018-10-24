5c18099ab6d16fae423b98cc04b76fcf20e80608
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-24 14:04:18 +0400

    Выводим только одну переменную автора

--- app/views/news/newsinfo.php
+++ app/views/news/newsinfo.php
@@ -1,4 +1,4 @@
 <h1><?=$news['title']?></h1>
 <div><?=date('d.m.Y H:i',$news['timestart'])?></div>
-<div>Автор: <a href="/user/id<?=$news['authorId']?>"><?=$news['authorFname'].' '.$news['authorLname']?></a></div>
+<div>Автор: <a href="/user/id<?=$news['authorId']?>"><?=$news['author']?></a></div>
 <div class="content"><?=$news['content']?></div>
