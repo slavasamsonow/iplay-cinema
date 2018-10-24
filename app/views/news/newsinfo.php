307f1779369403fa272f3b9b24a32dc7f8898650
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-24 15:18:44 +0400

    Revert "Если есть фото-выводится"
    This reverts commit 0d3499929d2b8fef1fe45e94881a60c7aa602307.

--- app/views/news/newsinfo.php
+++ app/views/news/newsinfo.php
@@ -1,22 +1,4 @@
-<h1>
-    <?=$news['title']?>
-</h1>
-<div class="row">
-    <? if($news['image']):?>
-    <div class="col-md-3">
-        <img src="/public/img/news/<?=$news['image']?>" alt="<?=$news['name']?>">
-    </div>
-    <? endif ?>
-    <div class="<?php echo ($news['image'])?'col-md-9':'col-md-12'?>">
-        <div>
-            <?=date('d.m.Y H:i',$news['timestart'])?>
-        </div>
-        <div>Автор: <a href="/user/id<?=$news['authorId']?>">
-                <?=$news['author']?></a></div>
-        <div class="content">
-            <?=$news['content']?>
-        </div>
-    </div>
-</div>
-<?php if($news['image']):?>
-<?php endif ?>
+<h1><?=$news['title']?></h1>
+<div><?=date('d.m.Y H:i',$news['timestart'])?></div>
+<div>Автор: <a href="/user/id<?=$news['authorId']?>"><?=$news['author']?></a></div>
+<div class="content"><?=$news['content']?></div>
