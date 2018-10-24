35cfc8413ea8054a898dde91f25e2d2b7cefaee2
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-24 13:43:59 +0400

    Модель новостей

---
+++ app/models/News.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace app\models;
+
+use app\core\Model;
+
+class News extends Model{
+    public function newsList(){
+        $news = $this->db->row('SELECT * FROM news n WHERE n.active = 1 ORDER BY n.timestart DESC');
+        return $news;
+    }
+
+    public function newsInfo($newsid){
+        $params = [
+            'id' => $newsid
+        ];
+        $news = $this->db->row('SELECT n.*, u.fname AS authorFname, u.lname AS authorLname, u.id AS authorId FROM news n JOIN users u ON n.author = u.id WHERE n.id = :id', $params);
+        if(empty($news)){
+            return false;
+        }
+        return $news[0];
+    }
+}
