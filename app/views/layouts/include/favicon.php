34b86aec5dd0f21416c88ee43650f466f91af3b9
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-27 15:54:18 +0400

    Стили фавиконов в отдельном файле

---
+++ app/views/layouts/include/favicon.php
@@ -0,0 +1,12 @@
+<link rel="apple-touch-icon" sizes="180x180" href="/public/favicon/apple-touch-icon.png?v=1">
+<link rel="icon" type="image/png" sizes="32x32" href="/public/favicon/favicon-32x32.png?v=1">
+<link rel="icon" type="image/png" sizes="16x16" href="/public/favicon/favicon-16x16.png?v=1">
+<link rel="manifest" href="/public/favicon/site.webmanifest?v=1">
+<link rel="mask-icon" href="/public/favicon/safari-pinned-tab.svg?v=1" color="#280d42">
+<link rel="shortcut icon" href="/public/favicon/favicon.ico?v=1">
+<meta name="apple-mobile-web-app-title" content="Продюсерский центр ИГРА">
+<meta name="application-name" content="Продюсерский центр ИГРА">
+<meta name="msapplication-TileColor" content="#dd4433">
+<meta name="msapplication-TileImage" content="/public/favicon/mstile-144x144.png?v=1">
+<meta name="msapplication-config" content="/public/favicon/browserconfig.xml?v=1">
+<meta name="theme-color" content="#280d42">
