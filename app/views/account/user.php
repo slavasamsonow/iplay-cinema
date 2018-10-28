8639a6f62eeb6ac2a1c631c6c6b70e8db8b6d022
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-29 00:14:26 +0400

    Условия не выводить информацию

--- app/views/account/user.php
+++ app/views/account/user.php
@@ -5,19 +5,30 @@
 <div class="row">
     <?php if(!empty($userPage['photo'])): ?>
     <div class="col-md-3">
-        <img src="/public/img/users/<?=$userPage['photo']?>" alt="<?=$userPage['fname'].' '.$userPage['lname'].' | Продюсерский центр ИГРА'?>" width="200">
+        <img src="/public/img/users/<?=$userPage['photo']?>" alt="<?=$userPage['fname'].' '.$userPage['lname'].' | Продюсерский центр ИГРА'?>"
+            width="200">
     </div>
     <div class="col-md-9">
         <?php else: ?>
         <div class="col-md-12">
             <?php endif ?>
-            <div class="about"><?=$userPage['about']?></div>
-            <div class="projects">
-            <h2>Проекты:</h2>
-            <?php foreach($userProjects as $userProject):?>
-                <a href="/project/<?=$userProject['id']?>"><?=$userProject['name']?></a>
-            <?php endforeach ?>
+            <?php if(!$userPage['public'] && !isset($_SESSION['user'])):?>
+            Пользователь ограничил доступ к информации
+            <?php else: ?>
+            <div class="about">
+                <?=$userPage['about']?>
             </div>
+            <div class="projects">
+                <h2>Проекты:</h2>
+                <?php foreach($userProjects as $userProject):?>
+                <div class="project">
+                    <a href="/project/<?=$userProject['id']?>">
+                        <?=$userProject['name']?></a>
+                </div>
+
+                <?php endforeach ?>
+            </div>
+            <? endif ?>
 
         </div>
     </div>
