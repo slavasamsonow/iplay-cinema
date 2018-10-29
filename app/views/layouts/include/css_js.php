85ab261e740221ecc2605b3e3ed9b5ce98239596
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-29 12:43:46 +0400

    Обновлен файл скриптов

--- app/views/layouts/include/css_js.php
+++ app/views/layouts/include/css_js.php
@@ -6,4 +6,4 @@
 <script src="/public/js/jquery.js"></script>
 <script src="/public/js/jquery.maskedinput.min.js"></script>
 <script src="/public/js/owl.carousel.min.js"></script>
-<script src="/public/js/main.js?v=0.12.1"></script>
+<script src="/public/js/main.js?v=0.12.4"></script>
