254c1cc941b6e4c180f2f7a552a67aec18ec05d4
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-29 12:37:48 +0400

    Страница редактирования промокода

---
+++ app/views/admin/editpromocode.php
@@ -0,0 +1,53 @@
+<h1>Редактировать промокод</h1>
+<div class="row">
+    <form action="/admin/editpromocode/<?=$promocode['id']?>" method="post" class="col-md-6">
+        <input type="hidden" name="promocodeid" value="<?=$promocode['id']?>">
+        <div class="control-group form-group">
+            <label>Код</label>
+            <input type="text" class="form-control" name="code" required="true" value="<?=$promocode['code']?>">
+        </div>
+        <div class="control-group form-group">
+            <label>Скидка (в рублях или %)</label>
+            <input type="text" class="form-control" name="sale" required="true" value="<?=$promocode['sale']?>">
+        </div>
+        <div class="control-group form-group">
+            <label>Дата и время начала</label>
+            <input type="text" class="form-control" name="timestart" required="true" value="<?=date('d.m.Y H:i:m', $promocode['timestart'])?>">
+        </div>
+        <div class="control-group form-group">
+            <label>Дата и время конца</label>
+            <?php if($promocode['timeend'] == 0):?>
+            <input type="text" class="form-control" name="timeend" required="true" value="<?=date('d.m.Y H:i:', time())?>00">
+            <?php else: ?>
+            <input type="text" class="form-control" name="timeend" required="true" value="<?=date('d.m.Y H:i:m', $promocode['timeend'])?>">
+            <?php endif ?>
+        </div>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" name="timeend" value="0" <? if($promocode['timeend']==0){echo 'checked' ;}?>>
+                <span> Без конца </span>
+            </label>
+        </div>
+        <div class="control-group form-group">
+            <label>Курс:</label>
+            <select class="form-control" name="course">
+                <option value="all" <?if($promocode['course']=='all' ){echo 'selected' ;}?>>Все</option>
+                <?php foreach($coursesList as $coursesListItem):?>
+                <option value="<?=$coursesListItem['id']?>" <?if($promocode['course']==$coursesListItem['id']){echo
+                    'selected' ;}?>>
+                    <?=$coursesListItem['name']?>
+                </option>
+                <?php endforeach ?>
+            </select>
+        </div>
+        <div class="checkbox">
+            <label>
+                <input type="checkbox" name="active" value="1" <? if($promocode['active']==1){echo 'checked' ;}?>>
+                <span> Активный </span>
+            </label>
+        </div>
+        <div class="control-group form-group">
+            <button type="submit" class="btn btn-default btn-sm">Редактировать промокод</button>
+        </div>
+    </form>
+</div>
