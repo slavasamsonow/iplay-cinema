c8b42ad5eb1a492e517476493f4375661d9dfd22
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-29 12:42:42 +0400

    Навигационные ссылки в левом меню

--- app/views/layouts/include/left-menu.php
+++ app/views/layouts/include/left-menu.php
@@ -17,21 +17,14 @@
     <?php if($_SESSION['user']['role'] == 'admin'):?>
     <li class="parent"><span>Админ</span>
         <ul>
-            <li class="parent"><span>Проекты</span>
-                <ul>
-                    <li><a href="/admin/projects">Список всех проектов</a></li>
-                </ul>
-            </li>
+            <li><a href="/admin/projects">Проекты</a></li>
+            <li><a href="/admin/newslist">Новости</a></li>
+            <li><a href="/admin/promocodelist">Промокоды</a></li>
             <li class="parent"><span>Курсы</span>
                 <ul>
                     <li><a href="/admin/confirmtasks">Проверка заданий</a></li>
                     <li><a href="/admin/courses">Список всех курсов</a></li>
                     <li><a href="/admin/usercourses">Ученики в курсах</a></li>
-                </ul>
-            </li>
-            <li class="parent"><span>Новости</span>
-                <ul>
-                    <li><a href="/admin/newslist">Список всех новостей</a></li>
                 </ul>
             </li>
         </ul>
