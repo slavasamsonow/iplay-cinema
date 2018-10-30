e158718054f21e0b7492a9bbd45b10a1d553cf57
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-30 17:14:21 +0400

    Страница админа

---
+++ app/views/admin/index.php
@@ -0,0 +1,12 @@
+<ul>
+    <li><a href="/admin/projects">Проекты</a></li>
+    <li><a href="/admin/newslist">Новости</a></li>
+    <li><a href="/admin/promocodelist">Промокоды</a></li>
+    <li class="parent"><span>Курсы</span>
+        <ul>
+            <li><a href="/admin/confirmtasks">Проверка заданий</a></li>
+            <li><a href="/admin/courses">Список всех курсов</a></li>
+            <li><a href="/admin/usercourses">Ученики в курсах</a></li>
+        </ul>
+    </li>
+</ul>
