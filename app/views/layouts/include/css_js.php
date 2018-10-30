4fc48c8eefb8f79f4b45a767f47dee79a1fb5707
slavasamsonow <slavasamsonow@yandex.ru>
2018-10-30 23:50:53 +0400

    Файл css

--- app/views/layouts/include/css_js.php
+++ app/views/layouts/include/css_js.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="/public/css/bootstrap.min.css">
 <link rel="stylesheet" href="/public/css/default.css?v=0.13.0">
-<link rel="stylesheet" href="/public/css/main.css?v=0.13.0">
+<link rel="stylesheet" href="/public/css/main.css?v=0.13.2">
 <link rel="stylesheet" href="/public/css/media.css?v=0.7.0">
 
 <script src="/public/js/jquery.js"></script>
