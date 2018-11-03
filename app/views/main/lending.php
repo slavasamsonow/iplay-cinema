3f7361e0827bdda2e110467c39308da996c4e29c
slavasamsonow <slavasamsonow@yandex.ru>
2018-11-03 22:46:09 +0400

    Верстка страницы лендинга

--- app/views/main/lending.php
+++ app/views/main/lending.php
@@ -1,1 +1,370 @@
-<h1>Лендинг</h1>
+<div class="lending white-text">
+    <div class="container-fluid">
+        <div class="row">
+            <h1>Интенсив-шоу 2</h1>
+        </div>
+        <div class="row">
+            <div class="description col-md-8 col-md-offset-2">
+                Создай фильм за вечер. Найди свой путь в мире кино.
+                <hr>
+                Узнай, как начать карьеру мастера видео.
+            </div>
+        </div>
+        <div class="local-date">
+            Москва, Кинотеатр Каро, online - трансляция <br>
+            19 ноября, 19:00
+        </div>
+        <div class="zapis">
+            <button class="btn btn-default">Записаться</button>
+        </div>
+
+    </div>
+
+    <div class="background">
+        <img src="/public/img/lending/background-intensiv.png" alt="" style="position: relative; top: 10vh;">
+    </div>
+</div>
+
+<div class="who">
+    <div class="container-fluid">
+        <h2>Приглашаем на шоу:</h2>
+        <div class="list">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <h3>Кто ищет дело жизни</h3>
+                            <p>или разочарован текущей профессией. За 5 часов работы над фильмом под руководством
+                                профессионалов вы получите представление о 10 киноспециальностях. Включаясь во все этапы
+                                съемки, поймете, какая роль на площадке вас привлекает. Эксперты расскажут, как начать
+                                карьеру в киноиндустрии.</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h3>Творческих людей</h3>
+                            <p>интересующихся новыми направлениями развития. Если вы уже создаете видео, ведете блог —
+                                обменяйтесь фишками с нашими экспертами. Получите новый опыт. В непринужденной,
+                                творческой
+                                атмосфере познакомьтесь с интересными людьми. Найдите новые идеи или команду для
+                                реализации
+                                имеющихся.</p>
+                        </div>
+                        <div class="col-md-4">
+                            <h3>Любителей кино</h3>
+                            <p>и тех, кто хочет создавать свое видео. Примите участие в съемочном процессе. Узнайте, как
+                                создать видеопродукт от идеи до результата. Познакомьтесь с практикующими мастерами
+                                империи
+                                кино. Получите вдохновение и инструменты для реализации идей.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="invest">
+    <div class="container-fluid">
+        <h2>Инвестиция в будущее</h2>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                Среди участников <b>Интенсив-шоу №1</b> были те, кто решился на перемены. <br>
+                Они уже начали путь к <b>новой профессии</b>. Выделите время на то, что поможет и вам <b>изменить
+                    жизнь</b>. Найдите
+                за вечер профессию, что приведет вас к <b>самореализации, успеху, славе и финансовой уверенности</b>.
+                <br>
+                А люди, с которыми познакомитесь, в дальнейшем могут стать <b>вашей командой</b>.
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="plan">
+    <div class="container-fluid">
+        <h2>План мероприятия</h2>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="item"><img src="/public/img/lending/plan/intensiv-1.png" alt="">
+                    <div>1. Знакомство</div>
+                </div>
+                <div class="item"><img src="/public/img/lending/plan/intensiv-2.png" alt="">
+                    <div>2. Выступление экспертов</div>
+                </div>
+                <div class="item"><img src="/public/img/lending/plan/intensiv-3.png" alt="">
+                    <div>3. Выбор идеи и <br>разработка сценария</div>
+                </div>
+                <div class="item"><img src="/public/img/lending/plan/intensiv-4.png" alt="">
+                    <div>4. Кастинг актеров</div>
+                </div>
+                <div class="item"><img src="/public/img/lending/plan/intensiv-5.png" alt="">
+                    <div>5. Съемка</div>
+                </div>
+                <div class="item"><img src="/public/img/lending/plan/intensiv-6.png" alt="">
+                    <div>6. Монтаж</div>
+                </div>
+                <div class="item"><img src="/public/img/lending/plan/intensiv-7.png" alt="">
+                    <div>7. Показ</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="request-block">
+    <div class="text">
+        Оставь заявку — сделай свой шаг к кинокарьере
+    </div>
+    <div class="button">
+        <button class="btn btn-primary">Записаться</button>
+    </div>
+</div>
+
+<div class="speakers white-text">
+    <h2>Наши спикеры:</h2>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+                <div class="col-md-4 item">
+                    <div class="image">
+                        <span>
+                            <img src="/public/img/lending/speakers/harlamov.png" alt="">
+                        </span>
+                    </div>
+                    <div class="name">
+                        <span>
+                            Александр Харламов
+                        </span>
+                    </div>
+                    <div class="description">
+                        продюсер, клипмейкер, режиссер
+                    </div>
+                    <div class="about">
+                        Стажировался в COMPANY3. <br>
+                        Работал над цветокоррекцией фильма <br>
+                        Майкла Бея "Кровью и потом. Анаболики". <br>
+                        В своем текущем проекте «Роман дня и ночи» <br>
+                        выступает как продюсер, режиссер и автор сценария.
+                    </div>
+                </div>
+                <div class="col-md-4 item">
+                    <div class="image">
+                        <span>
+                            <img src="/public/img/lending/speakers/gluhov.png" alt="">
+                        </span>
+                    </div>
+                    <div class="name">
+                        <span>
+                            Дмитрий Глухов
+                        </span>
+                    </div>
+                    <div class="description">
+                        ведущий
+                    </div>
+                    <div class="about">
+                        Участвовал в проекте Владимира Познера <br>
+                        «Школа живых городов. <br>
+                        Вел эфиры Радио Адам 104.5, STUD.Fm радио УдГУ. <br>
+                        Направлял творческий процесс Интенсив-шоу №1.
+                    </div>
+                </div>
+                <div class="col-md-4 item">
+                    <div class="image">
+                        <span>
+                            <img src="/public/img/lending/speakers/kupr.png" alt="">
+                        </span>
+                    </div>
+                    <div class="name">
+                        <span>
+                            Егор Куприянов
+                        </span>
+                    </div>
+                    <div class="description">
+                        актер театра
+                        и кино
+                    </div>
+                    <div class="about">
+                        Главная роль в короткометражном сериале
+                        «А зомби здесь тихие». Сценарист, продюсер
+                        и режиссер «Русские девушки не сдаются»
+                        и «Который год лежу я здесь на рубеже».
+                        Финалист шоу «Минута славы» «Человек
+                        без костей». Автор курса по актерскому
+                        мастерству "Импровизация — основа жизни".
+                        Тренер голоса, произношения, эстрадного вокала.
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 item">
+                    <div class="image">
+                        <span>
+                            <img src="/public/img/lending/speakers/abalmasov.png" alt="">
+                        </span>
+                    </div>
+                    <div class="name">
+                        <span>
+                            Антон Абалмасов
+                        </span>
+                    </div>
+                    <div class="description">
+                        оператор
+                    </div>
+                    <div class="about">
+                        Основатель продакшн "Пассажиры", клипмейкер,
+                        оператор постановщик. Снимал клипы
+                        для АК-47, Смоки МО, Триагрутрика.
+                    </div>
+                </div>
+                <div class="col-md-4 item">
+                    <div class="image">
+                        <span>
+                            <img src="/public/img/lending/speakers/koshkin.png" alt="">
+                        </span>
+                    </div>
+                    <div class="name">
+                        <span>
+                            Юрий Кошкин
+                        </span>
+                    </div>
+                    <div class="description">
+                        Монтажер
+                    </div>
+                    <div class="about">
+                        Опыт монтажа полнометражных фильмов
+                        и видеоклипов. Монтирует ролики в Like Центре.
+                    </div>
+                </div>
+                <div class="col-md-4 item">
+                    <div class="image">
+                        <span>
+                            <img src="/public/img/lending/speakers/hachaturyanc.png" alt="">
+                        </span>
+                    </div>
+                    <div class="name">
+                        <span>
+                            Виталий Хачатурянц
+                        </span>
+                    </div>
+                    <div class="description">
+                        режисер, оператр
+                        постановщик
+                    </div>
+                    <div class="about">
+                        Снимал проекты для федеральных
+                        телеканалов: 1 Канал: “Где ты брат” (фильм
+                        о Бодрове-младшем), “Ледниковый период”;
+                        Культура “Атланты в поисках истины”;
+                        НТВ «Тайный шоу-бизнес», “Моя исповедь”;
+                        MTV special “Реалити”; ТВЦ «Прощание»,
+                        “Без обмана” и др.
+                        Режиссёр в компании “Up!Media”
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="project">
+    <h2>Проект, с которым будем работать</h2>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Хотите понять, как все это
+                        осуществимо на практике?
+                        Смотрите видео прошлого интенсива. </h3>
+                    <img src="/public/img/lending/video-zagl.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="price white-text">
+    <h2>Цены</h2>
+    <div class="prices">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <div class="row">
+                    <div class="col-md-5 item">
+                        <div class="name">До 11 ноября</div>
+                        <div class="value">1000 р.</div>
+                    </div>
+                    <div class="col-md-5 col-md-offset-2 item">
+                        <div class="name">До 18 ноября</div>
+                        <div class="value">1500 р.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="request">
+        Успей записаться по выгодной цене
+        <div class="button">
+            <button class="btn btn-default">Записаться</button>
+        </div>
+    </div>
+</div>
+
+<div class="cooperation">
+    <h2>Сотрудничество</h2>
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="name">
+                        Ищем партнеров в других городах
+                    </div>
+                    <div class="text">
+                        Свяжитесь с нами, если <br>
+                        • вы хотите организовать Интенсив-шоу в вашем городе <br>
+                        • интересуетесь возможностью организации киношколы
+                        в своем населенном пункте
+
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="name">
+                        Мы открыты к сотрудничеству
+                    </div>
+                    <div class="text">
+                        • Если вы хотите продвигать свой бренд или продукт в видеоформате <br>
+                        • Если хотите инвестировать в нас или оказать спонсорскую помощь <br>
+                        • Если готовы предложить свой вариант сотрудничества
+                        Оставьте заявку (нужна форма). Мы с вами свяжемся
+                        и договоримся об обоюдовыгодных условиях взаимодействия.
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="name">
+                        У вас есть опыт, которым вы хотите поделиться?
+                    </div>
+                    <div class="text">
+                        Рассмотрим кандидатов на роль экспертов
+                        Интенсив-шоу и преподавателей школы.
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="name">
+                        Ищем волонтеров
+                    </div>
+                    <div class="text">
+                        Если вы активный человек, и хотите помочь с организацией Интенсив-шоу
+                        (подготовка площадки, оборудование и др) оставьте заявку. Мы с вами свяжемся.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="button">
+        <button class="btn btn-primary">Оставить заявку</button>
+    </div>
+</div>
+
+<div class="contact">
+    <div class="container-fluid">
+        <div class="logo">
+            <img src="/public/img/content/logo-white.png" alt="">
+        </div>
+    </div>
+</div>
