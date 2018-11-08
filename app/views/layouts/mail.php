7efb0941b5e0d55d04a1760d3bfe11960c601810
slavasamsonow <slavasamsonow@yandex.ru>
2018-11-09 01:43:30 +0400

    Шаблон для отправки сообщений с сайта

---
+++ app/views/layouts/mail.php
@@ -0,0 +1,119 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Продюсерский центр Игра</title>
+</head>
+<body style="margin:0; padding:0;">
+<table width="100%" bgcolor="#eeeeee" cellpadding="0" cellspacing="0" border="0"
+       style="font-family: Georgia, sans-serif; font-size: 14px;">
+    <tbody>
+    <tr>
+        <td>
+            <table cellpadding="0" cellspacing="0" width="100%" border="0" align="center" style="max-width: 600px;">
+                <tbody>
+                <!-- PreHeader -->
+                <tr>
+                    <td
+                            style="font-family: Arial, sans-serif; font-size: 10px;color: #999999; padding: 10px 0; text-align: right">
+                        <!--                        Если письмо отображается некорректно, <a href="#"-->
+                        <!--                                                                 style="text-decoration: none; color: #6b0f41">нажмите-->
+                        <!--                        здесь</a>-->
+                    </td>
+                </tr>
+                <!-- Main -->
+                <tr>
+                    <td>
+                        <a href="https://iplay-cinema.ru?utm_medium=email&utm_source=email_site&utm_campaign=email_site&utm_content=other"
+                           target="_blank" style="text-decoration: none;">
+                            <img src="https://iplay-cinema.ru/public/img/mail/header.jpg" width="600" height="105"
+                                 alt="Продюсерский центр Игра"
+                                 style="display:block; border:0; margin:0; background: #eeeeee;">
+                        </a>
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#ffffff" style="padding: 30px;">
+                        <?=$content?>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#111">
+                        <img src="https://iplay-cinema.ru/public/img/mail/hr.jpg" alt="" width="100%"
+                             style="display: block; border: 0; margin: 0;">
+                    </td>
+                </tr>
+                <tr>
+                    <td bgcolor="#fff" style="padding: 30px">
+
+                        <table cellpadding="0" cellspacing="0" border="0" width="100%">
+                            <tbody>
+                            <tr valign="top">
+                                <td>
+                                    <a href="https://vk.com/iplay_videolab"
+                                       style="float:left; width: 30px; margin:0 30px 10px 0;">
+                                        <img src="https://iplay-cinema.ru/public/img/mail/vk.png" width="100%"
+                                             alt="vk"
+                                             style="display:block; margin:0; border:0;">
+                                    </a>
+                                    <a href="https://www.instagram.com/igra.pro.centre/"
+                                       style="float:left; width: 30px; margin:0 30px 10px 0;">
+                                        <img src="https://iplay-cinema.ru/public/img/mail/inst.png" width="100%"
+                                             alt="inst"
+                                             style="display:block; margin:0; border:0;">
+                                    </a>
+                                    <a href="https://www.facebook.com/iplayschool/"
+                                       style="float:left; width: 30px; margin:0 30px 10px 0;">
+                                        <img src="https://iplay-cinema.ru/public/img/mail/facebook.png" width="100%"
+                                             alt="inst"
+                                             style="display:block; margin:0; border:0;">
+                                    </a>
+                                    <a href="https://www.youtube.com/channel/UCU3nufuGyM-ETQ7WpQmp0Pg"
+                                       style="float:left; width: 40px; margin:0 30px 10px 0;">
+                                        <img src="https://iplay-cinema.ru/public/img/mail/youtube.png" width="100%"
+                                             alt="inst"
+                                             style="display:block; margin:0; border:0;">
+                                    </a>
+                                    <p style="margin:0; font-weight:bold; clear:both; font-size:12px; line-height:22px;">
+                                        <a href="https://iplay-cinema.ru?utm_medium=email&utm_source=email_site&utm_campaign=email_site&utm_content=other"
+                                           style="font-family: Arial, arial, sans-serif; text-decoration: none; color: #6b0f41">Перейти
+                                            на сайт</a>
+                                    </p>
+                                </td>
+                                <td width="30"><p style="margin:0; font-size:1px; line-height:1px;">
+                                        &nbsp;</p></td>
+                                <td style="font-family: Arial, sans-serif; text-align: right; line-height:30px;">
+                                    <a href="mailto: videolab.play@gmail.com"
+                                       style="display: block; text-decoration: none; color: #6b0f41">videolab.play@gmail.com</a>
+                                    <a href="tel:+79522271003"
+                                       style="display: block; text-decoration: none; color: #6b0f41">+7 (952)
+                                        227-10-03</a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 20px 0 0; font-family: Arial, sans-serif;" colspan="3">
+                                    <p style="margin:0; font-size:13px; font-weight: bold; text-align: center;">
+                                        &#169; 2018 Продюсерский центр Игра
+                                    </p>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <!-- end content -->
+                    </td>
+
+
+                </tr>
+                <!-- marginBottom -->
+                <tr>
+                    <td style="padding: 10px"></td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
