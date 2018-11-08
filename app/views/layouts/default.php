376c80151f1af0c98c9f10ce9fd6e4d73649cec4
slavasamsonow <slavasamsonow@yandex.ru>
2018-11-09 02:29:40 +0400

    Всплывающее окно лид-магнит

--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -1,67 +1,91 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>
-            <?=$seo['title']?>
-        </title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>
+        <?=$seo['title']?>
+    </title>
 
-        <meta name="description" content="<?=$seo['description']?>">
+    <meta name="description" content="<?=$seo['description']?>">
 
-        <?php require_once('include/favicon.php')?>
-        <?php require_once('include/css_js.php')?>
+    <?php require_once('include/favicon.php') ?>
+    <?php require_once('include/css_js.php') ?>
 
-        <?php require_once('include/yandexmetrika.php')?>
-        <?php require_once('include/googleanalitics.php')?>
-        <?php require_once('include/pixelvk.php')?>
+    <?php require_once('include/yandexmetrika.php') ?>
+    <?php require_once('include/googleanalitics.php') ?>
+    <?php require_once('include/pixelvk.php') ?>
 
-    </head>
+</head>
 
-    <body>
-        <div class="wrapper">
-            <?php require_once('include/left-menu.php')?>
-            <?php require_once('include/navbar.php')?>
+<body>
+<div class="wrapper">
+    <?php require_once('include/left-menu.php') ?>
+    <?php require_once('include/navbar.php') ?>
 
-            <div class="content">
-                <div class="container-fluid">
-                    <?=$content?>
-                </div>
-            </div>
-
-            <footer>
-                <div class="container-fluid">
-                    <a href="/public/docs/protect_policy_of_personal_information.pdf" target="_blank">Политика
-                        конфеденциальности</a>
-                </div>
-            </footer>
-
-            <div class="modal message">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <div class="modal-header"></div>
-                <div class="modal-body"></div>
-            </div>
-
-            <div class="modal leadMagnet">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <div class="modal-header">
-                </div>
-                <div class="modal-body">
-                    <p><b>Хочешь узнать, кто ты на съемочной площадке? Самовлюбленный актер, идейный сценарист или злой режиссер?</b></p>
-                    <a href="/tests/yourprofession" class="btn btn-default">Кликай сюда</a>
-                </div>
-            </div>
-
-            <div id="overlay"></div>
+    <div class="content">
+        <div class="container-fluid">
+            <?=$content?>
         </div>
+    </div>
 
-        <div class="process-load">
-            <div class="right-top">
-                <img src="/public/img/load.svg" alt="">
-            </div>
+    <footer>
+        <div class="container-fluid">
+            <a href="/public/docs/protect_policy_of_personal_information.pdf" target="_blank">Политика
+                конфеденциальности</a>
         </div>
-    </body>
+    </footer>
+
+    <div class="modal message">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <div class="modal-header"></div>
+        <div class="modal-body"></div>
+    </div>
+
+    <div class="modal leadMagnet">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <div class="modal-header">
+        </div>
+        <div class="modal-body">
+            <p><b>Хочешь узнать, кто ты на съемочной площадке? Самовлюбленный актер, идейный сценарист или злой
+                    режиссер?</b></p>
+            <p style="text-align: center;"><a href="/tests/yourprofession" class="btn btn-default">Кликай сюда</a></p>
+        </div>
+    </div>
+    <script>
+        function openModal(type, header, body) {
+            $('.modal').fadeOut(300);
+            if (header != undefined) {
+                $('.modal.' + type + ' .modal-header').html(header);
+            }
+            if (body != undefined) {
+                $('.modal.' + type + ' .modal-body').html(body);
+            }
+            $('body').addClass('modalopen');
+            $('.modal.' + type).fadeIn(300);
+            $('#overlay').fadeIn(300);
+        }
+
+        <?php if(!isset($_SESSION['leadMagnet'])): ?>
+        <?php $_SESSION['leadMagnet'] = true; ?>
+        $(document).ready(function () {
+            setTimeout(function () {
+                openModal('leadMagnet')
+            }, 12000);
+        })
+        <?php endif ?>
+    </script>
+
+    <div id="overlay"></div>
+</div>
+
+<div class="process-load">
+    <div class="right-top">
+        <img src="/public/img/load.svg" alt="">
+    </div>
+</div>
+</body>
 
 </html>
