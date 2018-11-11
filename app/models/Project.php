6808623022005764872ea10ae259ca0d045b9950
slavasamsonow <slavasamsonow@yandex.ru>
2018-11-11 22:23:27 +0400

    Описание функций и todo

--- app/models/Project.php
+++ app/models/Project.php
@@ -5,10 +5,24 @@ namespace app\models;
 use app\core\Model;
 
 class Project extends Model{
+    /**
+     * Список проектов
+     *
+     * @return array
+     */
+    // todo Переименовать в getActiveProjects
     public function projectsList(){
         return $this->db->row('SELECT p.*, u.id AS creatorid,u.fname AS creatorfname, u.lname AS creatorlname FROM projects p JOIN users u ON p.creator = u.id WHERE p.active = 1');
     }
 
+    /**
+     * Информация о проекте
+     *
+     * @param $projectid
+     *
+     * @return bool
+     */
+    // todo переименовать в getProjectInfo
     public function projectInfo($projectid){
         $params = [
             'id' => $projectid,
@@ -20,10 +34,25 @@ class Project extends Model{
         return $project[0];
     }
 
+    /**
+     * Возврат информации для редактировани
+     *
+     * @param $projectid
+     *
+     * @return mixed
+     */
+    // todo переименовать в getProjectEditInfo
     public function projectEditInfo($projectid){
         return $this->processTextOut($this->projectInfo($projectid));
     }
 
+    /**
+     * Создание проекта
+     *
+     * @param $indata
+     *
+     * @return string
+     */
     public function createProject($indata){
         $params = $this->processTextIn($indata);
         $params['timestart'] = time();
@@ -33,6 +62,14 @@ class Project extends Model{
         return $this->db->lastInsertId();
     }
 
+    /**
+     * Обноваление инфы о проекте
+     *
+     * @param $id
+     * @param $indata
+     *
+     * @return bool
+     */
     public function updateProject($id, $indata){
         $params = $this->processTextIn($indata);
         $paramNV = $this->db->paramNV($params);
