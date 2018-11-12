a55f0f48b812d3f1d9294ec7c0f0ccc9b3602e20
slavasamsonow <slavasamsonow@yandex.ru>
2018-11-12 18:56:24 +0400

    Подвал в отдельном файле

--- app/views/layouts/intro.php
+++ app/views/layouts/intro.php
@@ -31,52 +31,7 @@
         <?=$content?>
     </div>
 
-    <footer>
-        <div class="container-fluid">
-            <a href="/public/docs/protect_policy_of_personal_information.pdf" target="_blank">Политика
-                конфеденциальности</a>
-        </div>
-    </footer>
-    <div id="overlay"></div>
-    <div class="modal message">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <div class="modal-header"></div>
-        <div class="modal-body"></div>
-    </div>
-
-    <div class="modal leadMagnet">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <div class="modal-header">
-        </div>
-        <div class="modal-body">
-            <p><b>Хочешь узнать, кто ты на съемочной площадке? Самовлюбленный актер, идейный сценарист или злой
-                    режиссер?</b></p>
-            <p style="text-align: center;"><a href="/tests/yourprofession" class="btn btn-default">Кликай сюда</a></p>
-        </div>
-    </div>
-    <script>
-        function openModal(type, header, body) {
-            $('.modal').fadeOut(300);
-            if (header != undefined) {
-                $('.modal.' + type + ' .modal-header').html(header);
-            }
-            if (body != undefined) {
-                $('.modal.' + type + ' .modal-body').html(body);
-            }
-            $('body').addClass('modalopen');
-            $('.modal.' + type).fadeIn(300);
-            $('#overlay').fadeIn(300);
-        }
-
-        <?php if(!isset($_SESSION['leadMagnet'])): ?>
-        <?php $_SESSION['leadMagnet'] = true; ?>
-        $(document).ready(function () {
-            setTimeout(function () {
-                openModal('leadMagnet')
-            }, 12000);
-        })
-        <?php endif ?>
-    </script>
+    <?php require_once('include/footer_default.php') ?>
 </div>
 
 <div class="process-load">
