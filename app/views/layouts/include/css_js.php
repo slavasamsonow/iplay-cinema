ededae39779f850c43d4d190ce419a8feea3a2d0
slavasamsonow <slavasamsonow@yandex.ru>
2018-11-13 19:14:59 +0400

    Новые стили

--- app/views/layouts/include/css_js.php
+++ app/views/layouts/include/css_js.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="/public/css/bootstrap.min.css">
-<link rel="stylesheet" href="/public/css/default.css?v=0.16.1">
+<link rel="stylesheet" href="/public/css/default.css?v=0.16.2">
 <link rel="stylesheet" href="/public/css/main.css?v=0.16.1">
 <link rel="stylesheet" href="/public/css/media.css?v=0.15.7">
 
