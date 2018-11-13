c5375f7d576dc310ddf8e877a69e3ef02f6e7af0
slavasamsonow <slavasamsonow@yandex.ru>
2018-11-13 17:47:59 +0400

    Вывод списка преподавателей таблицей

--- app/views/admin/courseteachers.php
+++ app/views/admin/courseteachers.php
@@ -1,7 +1,30 @@
 <h1>Преподаватели курса "<?=$course['name']?>"</h1>
 
-<a href="teachers/add">Добавить преподавателя</a>
+<a href="teachers/add" class="btn">Добавить преподавателя</a>
 
-<?php
-print_r($teachers);
-?>
+<?php if(!empty($teachers)):?>
+<table class="teachers">
+    <tr>
+        <th>ФИО</th>
+        <th>Текст</th>
+        <th>Открепить</th>
+    </tr>
+    <?php foreach($teachers as $teacher): ?>
+        <tr>
+            <td>
+                <?=$teacher['fname'].' '.$teacher['lname']?>
+            </td>
+            <td>
+
+            </td>
+            <td>
+                <button class="btn" data-type="courseteacher" data-action="delete" data-course="<?=$course['id']?>" data-teacher="<?=$teacher['id']?>">
+                    Удалить
+                </button>
+            </td>
+        </tr>
+    <?php endforeach ?>
+</table>
+<?php else: ?>
+<p> На данный момент нет преподавателей</p>
+<?php endif ?>
