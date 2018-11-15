350db4465e0f933e6be4395ea4be5bd64923e253
slavasamsonow <slavasamsonow@yandex.ru>
2018-11-15 12:42:52 +0400

    Добавлены или переписаны методы добавления, изменения, списка всех и активных новостей, информации о новости и информации о новости для редактирования

--- app/models/News.php
+++ app/models/News.php
@@ -6,35 +6,116 @@ use app\core\Model;
 
 class News extends Model{
     /**
-     * Список активных новостей
+     * Создание новости
+     * @return string
+     */
+    public function create(){
+        $params = $this->processTextIn($indata);
+        $params['timestart'] = $this->toUnixTime($params['timestart']);
+        $paramNandV = $this->db->paramNandV($params);
+
+        $this->db->query('INSERT INTO news ('.$paramNandV['N'].') VALUES ('.$paramNandV['V'].')', $params);
+        return $this->db->lastInsertId();
+    }
+
+    /**
+     * Обновление новости
+     *
+     * @param $id
+     * @param $data
+     *
+     * @return bool
+     */
+    public function update($id, $data){
+        $params = $this->processTextIn($data);
+        $params['timestart'] = $this->toUnixTime($params['timestart']);
+        if(!isset($params['active'])){
+            $params['active'] = 0;
+        }
+        $paramNV = $this->db->paramNV($params);
+        $params['id'] = $id;
+
+        $this->db->query('UPDATE news SET '.$paramNV.' WHERE `id` = :id', $params);
+        return true;
+    }
+
+    /**
+     * Возвращает список новостей
      * @return array
      */
-    // todo перименовать в getActiveNews
-    // todo пагинация
-    public function newsList(){
-        $news = $this->db->row('SELECT * FROM news n WHERE n.active = 1 ORDER BY n.timestart DESC');
+    public function getAll(){
+        $news = $this->db->row('SELECT n.*, u.fname, u.lname, u.id AS authorId FROM news n JOIN users u ON n.author = u.id ORDER BY n.timestart DESC');
+        foreach($news as $key => $newsitem){
+            $news[$key]['author'] = $newsitem['fname'].' '.$newsitem['lname'];
+            unset($newsitem['fname'], $newsitem['lname']);
+        }
         return $news;
     }
 
     /**
-     * Информация новости
-     *
-     * @param $newsid
-     *
-     * @return array|bool
+     * Возвращает список активных новостей
+     * @return array
      */
-    // todo переименовать в getNewsInfo
-    public function newsInfo($newsid){
+    public function getActive(){
+        $news = $this->getAll();
+        foreach($news as $key => $newsitem){
+            if($newsitem['active'] == 0){
+                unset($news[$key]);
+            }
+        }
+        return $news;
+    }
+
+    /**
+     * Возвращает данные новости
+     *
+     * @param $id
+     *
+     * @return bool
+     */
+    public function getItem($id){
         $params = [
-            'id' => $newsid
+            'id' => $id,
         ];
         $news = $this->db->row('SELECT n.*, u.fname, u.lname, u.id AS authorId FROM news n JOIN users u ON n.author = u.id WHERE n.id = :id', $params);
         if(empty($news)){
             return false;
         }
+
         $news = $news[0];
         $news['author'] = $news['fname'].' '.$news['lname'];
         unset($news['fname'], $news['lname']);
+
         return $news;
+    }
+
+    public function getItemActive($id){
+        $params = [
+            'id' => $id,
+        ];
+        $news = $this->db->row('SELECT n.*, u.fname, u.lname, u.id AS authorId FROM news n JOIN users u ON n.author = u.id WHERE n.id = :id AND n.active = 1', $params);
+        if(empty($news)){
+            return false;
+        }
+
+        $news = $news[0];
+        $news['author'] = $news['fname'].' '.$news['lname'];
+        unset($news['fname'], $news['lname']);
+
+        return $news;
+    }
+
+    /**
+     * Возвращает данные новости для редактирования
+     *
+     * @param $id
+     *
+     * @return bool|mixed
+     */
+    public function getItemEdit($id){
+        if(!$news = $this->getItem($id)){
+            return false;
+        }
+        return $this->processTextOut($news);
     }
 }
